b5cd66f4a17fd02bc49c487979ef194c42dfab13
Fatima Siddique <fatimasidq@gmail.com>
2022-01-30 18:41:12 -0500

    tests and stuff

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,23 +1,24 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const interviewers = [];
   for (let interviewer of props.interviewers) {
     interviewers.push(
-        <InterviewerListItem
-          key={interviewer.id}
-          selected={interviewer.id === props.value}
-          setInterviewer={() => {
-            props.onChange(interviewer.id);
-          }}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-        ></InterviewerListItem>
+      <InterviewerListItem
+        key={interviewer.id}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => {
+          props.onChange(interviewer.id);
+        }}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      ></InterviewerListItem>
     );
   }
-
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -25,3 +26,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
